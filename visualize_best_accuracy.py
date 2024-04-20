e0f260e4cee596604d79f3fb71dce0ddd0b3af4c
Roman Braunstingl <rfj.braunstingl@gmail.com>
2024-04-20 11:05:38 +0200

    add visualization script

--- visualize_best_accuracy.py
+++ visualize_best_accuracy.py
@@ -1,24 +1,35 @@
 import matplotlib.pyplot as plt
 
 data_ref = [0.906988, 0.897612, 0.894296, 0.902943, 0.901524]
+data_fill1k = [0.888461, 0.893564, 0.890022]
+data_fill1k5 = [0.891564, 0.892088, 0.891365]
+data_fill2k = [0.882446]
+data_add0k5 = [0.899032, 0.891585, 0.891766]
+data_snfill1k = [0.894668, 0.894543, 0.89338]
+data_snadd0k5 = [0.889161]
 data_rc = [0.90013, 0.90122, 0.897742, 0.904631, 0.903449]
 
-# attempt 1
-# fig, axs = plt.subplots(1, 2)
-#
-# axs[0].boxplot(data_ref)
-# axs[0].set_title('reference')
-#
-# axs[1].boxplot(data_rc)
-# axs[1].set_title('random choice sampling')
-#
-# plt.show()
-
-# attempt 2
 _, ax = plt.subplots()
-data = [data_ref, data_rc]
+data = [
+    data_ref,
+    data_fill1k,
+    data_fill1k5,
+    data_fill2k,
+    data_add0k5,
+    data_snfill1k,
+    data_snadd0k5,
+    data_rc,
+]
 ax.boxplot(data)
-ax.set_xticklabels(['reference', 'random choice sampling'])
-# ax.set_title('reference', 'random choice sampling')
+ax.set_xticklabels([
+    'reference',
+    'fill1k',
+    'fill1k5',
+    'fill2k',
+    'add0k5',
+    'sn_f1k',
+    'sn_a0k5',
+    'rcs',
+])
 
 plt.show()
